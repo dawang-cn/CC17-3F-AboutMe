b3bf58a032309c8790498059b486cd2afc39ea1c
dawang-cn <XianCDawg@gmail.com>
2024-10-01 01:28:57 +0800

    git remote add origin https://github.com/bravo-jrs/cc17-3f-vagos.git

---
+++ CC173FAboutMe/app/src/main/java/com/example/cc17_3f_aboutme/adapter/AboutMeAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.cc17_3f_aboutme.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cc17_3f_aboutme.R
+import com.example.cc17_3f_aboutme.model.AboutMe
+
+class AboutMeAdapter(val data:List<AboutMe>):
+    RecyclerView.Adapter<AboutMeAdapter.AboutMeItemViewHolder>() {
+
+    class AboutMeItemViewHolder(val row: View): RecyclerView.ViewHolder(row){
+        val aboutMeImageView = row.findViewById<ImageView>(R.id.aboutMeItemImg)
+        val aboutMeNameTextView = row.findViewById<TextView>(R.id.aboutMeItemName)
+        val aboutMeDescTextView = row.findViewById<TextView>(R.id.aboutMeItemDesc)
+        val aboutMeShowMoreButton = row.findViewById<Button>(R.id.aboutMeItemShowMoreButton)
+    }
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AboutMeItemViewHolder {
+        val layout = LayoutInflater.from(parent.context).inflate(R.layout.about_me_item, parent, false)
+        return  AboutMeItemViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: AboutMeItemViewHolder, position: Int) {
+        holder.aboutMeImageView.setImageResource(data.get(position).imageResId)
+        holder.aboutMeNameTextView.text = data.get(position).name
+        holder.aboutMeDescTextView.text = data.get(position).desc
+        holder.aboutMeShowMoreButton.setOnClickListener(data.get(position).onClickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+
+    }
+}
