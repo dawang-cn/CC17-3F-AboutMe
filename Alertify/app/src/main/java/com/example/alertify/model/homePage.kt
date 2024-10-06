60efad10f93e2ba506c1f432e7e23c45c47fdf1b
dawang-cn <XianCDawg@gmail.com>
2024-10-07 02:10:04 +0800

    update

--- Alertify/app/src/main/java/com/example/alertify/model/homePage.kt
+++
@@ -1,6 +0,0 @@
-package com.example.alertify.model
-
-import android.view.View.OnClickListener
-
-data class HomePage(val imageResId: Int, val name: String, val desc:String, val onClickListener: OnClickListener){
-}
